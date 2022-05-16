442bb890321b2a8bb809e16101d9c8a672f2a3b7
강욱 이 <lkwks@naver.com>
2022-05-16 22:11:27 +0900

    fixing bugs, not completed yet

--- mychess.js
+++ mychess.js
@@ -28,10 +28,31 @@ class Piece {
         this.is_enpassant = -1;
     }
     
+	
+	/*
+	
+	현재 구조
+	
+	(1) W팀 기물 클릭
+	1. W팀 기물을 클릭하면 그 기물이 움직일 수 있는 경로를 뽑기 위해 possible_pos()가 실행된다.
+	2. possible_pos() 안에서는 그 기물이 움직일 수 있는 경로로 움직였을 때 B팀에 의해 우리 왕이 공격받지는 않는지 확인하기 위해 각 좌표마다 check_if_this_piece_can_avoid_check()가 실행된다.
+	3. check_if_this_piece_can_avoid_check()에서는 B팀의 각 기물이 W팀 왕을 공격할 수 있는지 확인하기 위해 각 기물마다 possible_pos()가 실행된다. 단, 이때 실행되는 possible_pos()는 this.team과 this.board.now_clicked.tema이 다르기 때문에 다시 check_if_this_piece_can_avoid_check()가 실행되지는 않는다. (무한루프 방지)
+	
+	(2) W팀 기물을 이동 경로로 이동시킴
+	1. 이동시킬 좌표를 클릭하면 이동 연산을 수행하기 위해 move_to()가 실행되며, 이후 이동 연산을 끝마친 후 그 기물이 혹시 B팀 왕을 공격하지 않는지, 혹시 스테일메이트가 되진 않았는지 확인하기 위해 check_if_there_is_check_and_mates()가 실행된다.
+	2. check_if_there_is_check_and_mates()에서는...
+	- 체크인지 확인하기 위해 막 이동한 W팀 말이 이동할 수 있는 모든 경로를 체크한다. 이때 실행되는 possible_pos()는 this.team과 this.board.now_clicked.team이 같은데, 그럼에도 불구하고 그 말이 공격을 했을 때 W팀 왕이 공격당하는지 여부를 확인하는 check_if_this_piece_can_avoid_check()는 실행되어선 안된다 ㅡㅡ
+	- W팀 기물 이동 완료 후 움직일 수 있는 B팀 기물이 있는지(스테일메이트가 아닌지) 확인하기 위해 B팀 기물에 대해 possible_pos()가 실행된다. 단 이때 실행되는 possible_pos()는 this.team과 this.board.now_clicked.team이 다르기 때문에 여기서 추가로 check_if_this_piece_can_avoid_check()가 실행되지는 않는다. (무한루프 방지)
+	
+	*/
+	
+	
     possible_pos() //현재 기물이 움직일 수 있는 경로를 모두 뽑아 리턴
     {
         var arr = [], col = this.pos[1], row = this.pos[0], moving_piece, r, c, loop_n;
     
+		console.log(this.team, this.board.now_clicked.team);
+	
         piece_dir[this.name].forEach( elem => {
             r = row + elem[0] * (this.team == 'W'?1:-1);
             c = col + elem[1];
@@ -53,12 +74,9 @@ class Piece {
                     if (not_available == false)
                         arr.push([r, c]);
                 }
-                else if (this.board.checked && this.board.checked.team == this.board.now_clicked.team)
-                {
-                    if (this.check_if_this_piece_can_avoid_check([r, c])) //체크 상태고 지금 클릭한 게 왕이 아니라면 '체크한 말'을 죽이거나 경로를 막는 선택만 가능
-                        arr.push([r, c]);
-                }
-                else 
+                else if (this.team != this.board.now_clicked.team || (this.team == this.board.now_clicked.team && ((this.pgn_n > 0 &&this.pgn.length > this.pgn_n) || this.check_if_this_piece_can_avoid_check([r, c]))))
+					//체크 상태라면 체크 상태를 이기는 수만 둘 수 있고. 지금 체크상태가 아니더라도 [r, c]로 움직여서 체크 될 상황이면 움직이면 안됨.
+					//만약 지금 클릭한 기물의 이동 경로를 찾는 게 아니라, 클릭된 기물을 움직였을 때 혹시 체크 되는 건 아닌지 확인하기 위해 상대편 기물의 이동경로를 계산하는 상황이라면, '이 기물 움직여서 체크 되는 거 아냐?' 경우는 고려 필요 x. 무한루프 주의.
                     arr.push([r, c]);
                 if (this.board.board[r][c]) break;
                 r += elem[0] * (this.team == 'W'?1:-1);
@@ -74,7 +92,7 @@ class Piece {
                 c = col + elem[1];
                 if (r <= 7 && r >= 0 && c <= 7 && c >= 0)
                 {
-                    if (this.board.board[r][c] && this.board.board[r][c].team != this.team && (!this.board.checked || (this.board.checked && this.board.checked.team == this.board.now_clicked.team && this.check_if_this_piece_can_avoid_check([r, c])))) //폰의 공격
+                    if (this.board.board[r][c] && this.board.board[r][c].team != this.team && (this.team != this.board.now_clicked.team  || (this.team == this.board.now_clicked.team && this.check_if_this_piece_can_avoid_check([r, c])))) //폰의 공격
                         arr.push([r, c]);
                     if (this.board.board[row][c] && this.board.board[row][c].name == "P" && this.board.board[row][c].team != this.team && this.board.board[row][c].is_enpassant >= 0) //앙파상
                         arr.push([r, c]);
@@ -111,24 +129,36 @@ class Piece {
         
         return killed_piece;
     }
+	
+	
+	/*
+	
+	possible_pos() 함수를 여러개 만들어야 하는 이유
+	
+	1. 지금 현재 기물을 움직이기 위해 click했을 때 보드에 찍힐 경로를 출력하는 함수
+	2. 1번을 출력하에 앞서, 혹시나 click된 기물을 어딘가로 움직였을 때 이로 인해 상대 기물에게 우리 왕이 공격받는 건 아닌지 체크하는 함수
+	- 2번을 체크하기 위해, click된 기물을 어딘가로 움직인 상태에서 상대 기물이 우리 왕을 공격하는건 아닌지 체크하는 함수. 지금까지는 이 함수를 1번 함수랑 같은 걸 써서 무한루프 걸렸는데 같은거 쓰면 당연 안됨..
+	
+	
+	*/
+	
+	
 
     check_if_this_piece_can_avoid_check(pos)
     {
-        //this를 pos로 옮기면 체크를 피할 수 있는지 확인하는 메서드
+        //this를 pos로 옮기면 체크를 피할 수 있는지, 또는 pos로 옮겨서 this.team의 왕이 체크 상태가 되는건 아닌지 확인하는 메서드
         
-        var king = this.board.checked, avoid_succeed=true, temp = this.board.board[pos[0]][pos[1]];
-        this.board.checked = false;
-        
+        var avoid_succeed=true, temp = this.board.board[pos[0]][pos[1]];
         this.board.board[pos[0]][pos[1]] = this;
+		
+		console.log(this);
         
         for (var i=0; i<8; i++)
             for (var j=0; j<8; j++)
-                if (this.board.board[i][j] && this.board.board[i][j].team != this.team && is_in(this.board.board[i][j].possible_pos(), king.pos))
+                if (this.board.board[i][j] && this.board.board[i][j].team != this.team && avoid_succeed && is_in(this.board.board[i][j].possible_pos(), this.board.king[this.team].pos)) //현재 [i, j] 위치에 있는 상대편 기물이 혹시나 this.team의 왕을 공격하지 않는지?
                     avoid_succeed = false;
 
-        console.log(avoid_succeed);
         this.board.board[pos[0]][pos[1]] = temp;
-        this.board.checked = king;
         return avoid_succeed;
     }
 
@@ -137,7 +167,7 @@ class Piece {
         var avoid_check = true;
         for (var i=0; i<8; i++)
             for (var j=0; j<8; j++)
-                if (this.board.board[i][j] && this.board.board[i][j].team != this.team)
+                if (this.board.board[i][j] && this.board.board[i][j].team != this.team && avoid_check)
                 {
                     var possible_pos = this.board.board[i][j].possible_pos();
                     if (is_in(possible_pos, pos) || is_in(possible_pos, [pos[0], pos[1] == 2 ? 3: 5])) //[i, j]가 킹의 이동경로를 공격한다면 캐슬 불가.
@@ -159,6 +189,7 @@ class Board {
         this.pgn = [];
         this.pgn_n = 0;
         this.now_clicked = null;
+		this.king = {'W':null, 'B':null};
 
 
         this.promo_box = new PromoteBox(this);
@@ -202,6 +233,8 @@ class Board {
                 {
                     this.board[i][j] = new Piece(this, board_initial_piece[i][j], [i, j]);
                     this.cell_dom_elems[i][j].appendChild(this.board[i][j].dom_elem);
+					if (board_initial_piece[i][j] == 'K')
+						this.king[i==0 ? 'W' : 'B'] = this.board[i][j];
                 }
                 else
                     this.board[i][j] = false;
@@ -265,7 +298,7 @@ class Board {
                     if (this.board[i][j].possible_pos().length > 0) //다른 기물로 체크 상태 피할 수 있으면 메이트 아님. 
                         is_checkmate = false;
                 }
-                else
+                else if(is_stalemate)
                 {
                     if (this.board[i][j].possible_pos().length > 0)
                         is_stalemate = false;                
@@ -460,8 +493,9 @@ document.getElementById("new_game").addEventListener("click", e=>board.new_game(
             board.pgn = board.pgn.slice(0, board.pgn_n);
             board.pgn_n = board.pgn.length;
         }
-        board.pgn.push([[row1, col1], board.now_clicked.pos, killed_piece]); board.pgn_n++;
+        board.pgn.push([[row1, col1], board.now_clicked.pos, killed_piece]); 
         board.check_if_there_is_check_and_mates();
+		board.pgn_n++;
         if (board.now_clicked.name == "P" && row2 == (board.now_clicked.team == "W"?7 : 0))
             board.promo_box.open_window(board.now_clicked);
         
